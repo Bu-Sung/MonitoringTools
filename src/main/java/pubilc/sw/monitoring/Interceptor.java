bcb18f00dc28ecf6ee9f655bcb5c87f0025ac11e
Bu-Sung <qntjd4680@gmail.com>
2023-08-20 01:58:00 +0900

    [ADD] 사용자 세션 유무 확인 Interceptor 적용

---
+++ src/main/java/pubilc/sw/monitoring/Interceptor.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pubilc.sw.monitoring;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ *
+ * @author qntjd
+ */
+public class Interceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        HttpSession session = request.getSession();
+        Object user = session.getAttribute("user");
+
+        if (user == null) {
+            // 세션에 'user' 속성이 없는 경우 로그인 페이지로 리다이렉트
+            response.sendRedirect("redirect:/");
+            return false;
+        }
+
+        return true; // 세션에 'user' 속성이 있는 경우 요청 처리 계속 진행
+    }
+}
